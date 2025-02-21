95dbea11ee29a46d817d23f82d884e57c3e73115
Kian Casutt <146694037+Kikrovo@users.noreply.github.com>
2025-02-21 04:09:05 -0500

    Update proj1.c

--- proj1.c
+++ proj1.c
@@ -3,16 +3,16 @@
 #include <string.h>
 #include <getopt.h>
 
-#define MAX_CHAR 256
+#define MAX_CHAR 256 // For Max possible number of ASCII values.
 
-// Make Node structures
+// Make Nodes to put in Huffman Tree.
 typedef struct Node {
     unsigned char character;
     int freq;
     struct Node *left, *right;
 } Node;
 
-// Make a min-heap for prio queue
+// Use priority-q to build tree. 
 typedef struct MiniHeap {
     int size;
     Node *array[MAX_CHAR];
@@ -20,73 +20,106 @@ typedef struct MiniHeap {
 
 int debug = 0;
 
-// Huffman encoding stages
+// Read input file and count character freqeuencies. 
 void count_frequencies(FILE *file, int freqs[]);
-MiniHeap *build_min_heap(int freqs[]);
+
+MiniHeap *create_mini_heap(int freqs[]);
+
+// Build huffman tree from mini-heap.
 Node *build_huffman_tree(MiniHeap *heap);
-void generate_codes(Node *root, char *code, int depth, char *codes[]);
+
+// Traverse tree and initiate huffman codes.
+void ini_codes(Node *root, char *code, int depth, char *codes[]);
+
+// Encode input file.
 void encode_file(FILE *infile, FILE *outfile, char *codes[]);
 
-// Helper functions for the min-heap
-void insert_heap(MiniHeap *heap, Node *node);
-Node *remove_min(MiniHeap *heap);
-void swap_nodes(Node **a, Node **b);
-void min_heapify(MiniHeap *heap, int index);
+// Free allocated memory.
+void free_tree(Node *root);
+void free_codes(char *codes[]);
 
+// Insert node(s) into queue.
+void insert_to_heap(MiniHeap *heap, Node *node);
+
+// Remove and return  node with smallest frequency from the mini-heap.
+Node *remove_mini(MiniHeap *heap);
+
+// Maintain heap after removing a node. 
+void mini_maintain(MiniHeap *heap, int index);
 
 int main(int argc, char *argv[]) {
     int opt;
-    char *input_filename = NULL, *output_filename = "output.huff";
+    char *inp_file = "completeShakespeare.txt";
+    char *out_file = "huffman.out";
     FILE *infile, *outfile;
-    
-    while ((opt = getopt(argc, argv, "d")) != -1) {
-        if (opt == 'd') debug = 1;
+
+    // Go through command-line arguments for input, output and debug.
+    while ((opt = getopt(argc, argv, "i:o:d")) != -1) {
+        switch (opt) {
+            case 'i':
+                inp_file = optarg;
+                break;
+            case 'o':
+                out_file = optarg;
+                break;
+            case 'd':
+                debug = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-i inputfile] [-o outputfile] [-d]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
-    
-    if (optind >= argc) {
-        fprintf(stderr, "Usage: %s [-d] inputfile\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    
-    input_filename = argv[optind];
-    infile = fopen(input_filename, "rb");
+
+    // Open input file.
+    infile = fopen(inp_file, "rb");
     if (!infile) {
         perror("Error opening input file");
         exit(EXIT_FAILURE);
     }
-    
+
+    // Counting frequencies in the file.
     int freqs[MAX_CHAR] = {0};
     count_frequencies(infile, freqs);
-    rewind(infile);
-    
-    MiniHeap *heap = build_min_heap(freqs);
+    rewind(infile); // Reset pointer to beginning.
+
+    // Building the Huffman tree.
+    MiniHeap *heap = create_mini_heap(freqs);
     Node *root = build_huffman_tree(heap);
-    
+
+    // Initiate Huffman codes for every character.
     char *codes[MAX_CHAR] = {NULL};
     char temp_code[MAX_CHAR];
-    generate_codes(root, temp_code, 0, codes);
-    
-    outfile = fopen(output_filename, "wb");
+    ini_codes(root, temp_code, 0, codes);
+
+    // Open output file.
+    outfile = fopen(out_file, "wb");
     if (!outfile) {
         perror("Error opening output file");
+        fclose(infile);
         exit(EXIT_FAILURE);
     }
+
     
     encode_file(infile, outfile, codes);
+
+    // Close files and free allocated memory. 
     fclose(infile);
     fclose(outfile);
-    
-    printf("Encoding complete. Output written to %s\n", output_filename);
+    free_tree(root);
+    free_codes(codes);
+
     return 0;
 }
 
+// Count the character frequencies in the open file.
 void count_frequencies(FILE *file, int freqs[]) {
     int ch;
     while ((ch = fgetc(file)) != EOF) {
         freqs[ch]++;
     }
     if (debug) {
-        printf("Character freqs:\n");
+        printf("Character Frequencies:\n");
         for (int i = 0; i < MAX_CHAR; i++) {
             if (freqs[i] > 0) {
                 printf("%c: %d\n", i, freqs[i]);
@@ -95,7 +128,8 @@ void count_frequencies(FILE *file, int freqs[]) {
     }
 }
 
-MiniHeap *build_min_heap(int freqs[]) {
+// Construct the prio-q from character frequency array.
+MiniHeap *create_mini_heap(int freqs[]) {
     MiniHeap *heap = malloc(sizeof(MiniHeap));
     heap->size = 0;
     for (int i = 0; i < MAX_CHAR; i++) {
@@ -104,27 +138,29 @@ MiniHeap *build_min_heap(int freqs[]) {
             node->character = i;
             node->freq = freqs[i];
             node->left = node->right = NULL;
-            insert_heap(heap, node);
+            insert_to_heap(heap, node);
         }
     }
     return heap;
 }
 
+// Generate Huffman tree from the mini-heap.
 Node *build_huffman_tree(MiniHeap *heap) {
     while (heap->size > 1) {
-        Node *left = remove_min(heap);
-        Node *right = remove_min(heap);
+        Node *left = remove_mini(heap);
+        Node *right = remove_mini(heap);
         Node *new_node = malloc(sizeof(Node));
         new_node->character = '\0';
         new_node->freq = left->freq + right->freq;
         new_node->left = left;
         new_node->right = right;
-        insert_heap(heap, new_node);
+        insert_to_heap(heap, new_node);
     }
-    return remove_min(heap);
+    return remove_mini(heap);
 }
 
-void generate_codes(Node *root, char *code, int depth, char *codes[]) {
+// Initiate Huffman codes for characters in our tree. 
+void ini_codes(Node *root, char *code, int depth, char *codes[]) {
     if (!root) return;
     if (!root->left && !root->right) {
         code[depth] = '\0';
@@ -132,11 +168,12 @@ void generate_codes(Node *root, char *code, int depth, char *codes[]) {
         if (debug) printf("%c: %s\n", root->character, codes[root->character]);
     }
     code[depth] = '0';
-    generate_codes(root->left, code, depth + 1, codes);
+    ini_codes(root->left, code, depth + 1, codes);
     code[depth] = '1';
-    generate_codes(root->right, code, depth + 1, codes);
+    ini_codes(root->right, code, depth + 1, codes);
 }
 
+// Encode file
 void encode_file(FILE *infile, FILE *outfile, char *codes[]) {
     rewind(infile);
     unsigned char buffer = 0;
@@ -157,5 +194,52 @@ void encode_file(FILE *infile, FILE *outfile, char *codes[]) {
     if (bit_count > 0) {
         buffer <<= (8 - bit_count);
         fwrite(&buffer, 1, 1, outfile);
+    }
+}
+
+
+void insert_to_heap(MiniHeap *heap, Node *node) {
+    int i = heap->size++;
+    while (i > 0 && heap->array[(i - 1) / 2]->freq > node->freq) {
+        heap->array[i] = heap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    heap->array[i] = node;
+}
+
+Node *remove_mini(MiniHeap *heap) {
+    Node *min_value = heap->array[0];
+    heap->array[0] = heap->array[--heap->size];
+    mini_maintain(heap, 0);
+    return min_value;
+}
+
+// Ensure heap property is still in effect. 
+void mini_maintain(MiniHeap *heap, int index) {
+    int smallest = index, left = 2 * index + 1, right = 2 * index + 2;
+    if (left < heap->size && heap->array[left]->freq < heap->array[smallest]->freq)
+        smallest = left;
+    if (right < heap->size && heap->array[right]->freq < heap->array[smallest]->freq)
+        smallest = right;
+    if (smallest != index) {
+        Node *temp = heap->array[index];
+        heap->array[index] = heap->array[smallest];
+        heap->array[smallest] = temp;
+        mini_maintain(heap, smallest);
+    }
+}
+
+// Freeing allocated memory functions for codes and tree. 
+void free_tree(Node *root) {
+    if (root) {
+        free_tree(root->left);
+        free_tree(root->right);
+        free(root);
+    }
+}
+
+void free_codes(char *codes[]) {
+    for (int i = 0; i < MAX_CHAR; i++) {
+        if (codes[i]) free(codes[i]);
     }
 }
